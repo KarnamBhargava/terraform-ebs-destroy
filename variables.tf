1a3fa3c61fc12d7321f880bd9180750bb63df3ca
KarthikNarsapur <karthik.n@cloudthat.com>
2025-04-24 15:21:50 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,13 +1,10 @@
-variable "bucket_name" {
-  type = string
-}
- 
+
 variable "region" {
   type    = string
   default = "ap-south-1"
 }
 
-variable "versioning" {
-  type    = bool
-  default = false
+variable "volumeId" {
+  type    = string
+
 }
